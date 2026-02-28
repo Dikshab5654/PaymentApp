2df856754b1b4acb0e34cec1e9dc7f8d829bc79f
Your Name <you@example.com>
2026-02-28 18:43:20 +0530

    Add PostgreSQL EF Migrations

---
+++ PaymentAPI/PaymentAPI/Migrations/20260228131319_InitialPostgres.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PaymentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentDetailId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
